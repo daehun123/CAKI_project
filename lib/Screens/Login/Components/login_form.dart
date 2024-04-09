71ecf3504a796f266724851ee8dff63af36c72ae
kangdaehun <crol0101@naver.com>
2024-04-09 16:08:51 +0900

    mkdir

---
+++ lib/Screens/Login/Components/login_form.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatelessWidget {
+  final login_formkey = GlobalKey<FormState>();
+
+  LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: login_formkey,
+      child: Column(
+        children: [
+          TextFormField(
+            textInputAction: TextInputAction.next,
+            obscureText: true,
+            cursorColor: const Color(0xFF8A9352),
+            decoration: const InputDecoration(
+              hintText: 'E-mail',
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Icon(Icons.email),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return '필수 입력 항목입니다.';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: const Color(0xFF8A9352),
+              decoration: const InputDecoration(
+                hintText: 'PassWord',
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '필수 입력 항목입니다.';
+                }
+                return null;
+              },
+            ),
+          ),
+          const SizedBox(height: 8.0),
+          Row(
+            children: [
+              const Spacer(),
+              Expanded(
+                flex: 8,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (login_formkey.currentState!.validate()) {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                              const Welcome_Screen()),
+                              (route) => false);
+                    }
+                  },
+                  child: Text('Login'.toUpperCase()),
+                ),
+              ),
+              const Spacer(),
+            ],
+          ),
+          const SizedBox(height: 16.0),
+        ],
+      ),
+    );
+  }
+}
