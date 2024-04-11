7f11d6d23be80ca789088c62b173bf4cd4876473
kangdaehun <crol0101@naver.com>
2024-04-11 11:15:26 +0900

    ãpre+

---
+++ lib/Screens/Preference/preference_screen.dart
@@ -0,0 +1,13 @@
+import 'package:caki_project/Components/background.dart';
+import 'package:flutter/material.dart';
+
+class pre_choice extends StatelessWidget {
+  const pre_choice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Background(
+      child: Text("관심사 선택"));
+  }
+}
+
