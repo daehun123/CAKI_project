bdfa99f0a26e93ec1e6b5b215e42e56f6bcb3481
taeuri <xodudwns6251@gmail.com>
2024-04-30 09:21:33 +0900

    필터 검색기능 수정

--- lib/Screens/search/FilterScreen.dart
+++ lib/Screens/search/FilterScreen.dart
@@ -6,11 +6,23 @@ import 'package:group_button/group_button.dart';
 import '../../Components/constants.dart';
 import '../Preference/preference_screen.dart';
 
-class FilterScreen extends StatelessWidget {
-  List<String> choice_list = []; // 사용자의 선택을 저장하는 리스트
-  int selectCount = 0;
+class FilterScreen extends StatefulWidget {
+  FilterScreen({Key? key}) : super(key: key);
 
-  FilterScreen({super.key});
+  @override
+  _FilterScreenState createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  List<String> choiceList = []; // 사용자의 선택을 저장하는 리스트
+  int selectCount = 0;
+  late GroupButtonController _controller; // GroupButtonController 선언
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = GroupButtonController(selectedIndexes: []);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,66 +41,62 @@ class FilterScreen extends StatelessWidget {
                   // 그룹 버튼을 가로로 스크롤 가능하게 만들기 위한 위젯
                   groupingType: GroupingType.wrap,
                   child: GroupButton(
-                      buttons: const [
-                        // 버튼 그룹의 옵션들
-                        '도수★★★',
-                        '당도★★★',
-                        '도수★★',
-                        '당도★★',
-                        '도수★',
-                        '당도★',
-                        '과일',
-                        '우유/크림',
-                        '탄산',
-                        '보드카',
-                        '럼',
-                        '브랜디',
-                        '위스키',
-                        '리큐르',
-                        '진',
-                        '데킬라'
-                      ],
-                      options: GroupButtonOptions(
-                        selectedShadow: const [],
-                        unselectedShadow: const [],
-                        selectedTextStyle: const TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                        selectedColor: kColor,
-                        // 선택된 버튼의 색상
-                        unselectedColor: Colors.grey,
-                        // 선택되지 않은 버튼의 색상
-                        unselectedTextStyle: const TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                        spacing: 5,
-                        // 버튼 사이의 간격
-                        buttonWidth: MediaQuery.of(context).size.width / 2.4,
+                    buttons: const [
+                      // 버튼 그룹의 옵션들
+                      '도수★★★',
+                      '당도★★★',
+                      '도수★★',
+                      '당도★★',
+                      '도수★',
+                      '당도★',
+                      '과일',
+                      '우유/크림',
+                      '탄산',
+                      '보드카',
+                      '럼',
+                      '브랜디',
+                      '위스키',
+                      '리큐르',
+                      '진',
+                      '데킬라'
+                    ],
+                    options: GroupButtonOptions(
+                      selectedShadow: const [],
+                      unselectedShadow: const [],
+                      selectedTextStyle: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
                       ),
-                      isRadio: false,
-                      controller: GroupButtonController(
-                        selectedIndexes: const [],
+                      selectedColor: kColor,
+                      // 선택된 버튼의 색상
+                      unselectedColor: Colors.grey,
+                      // 선택되지 않은 버튼의 색상
+                      unselectedTextStyle: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
                       ),
-                      onSelected: (val, i, selected) => {
-                            debugPrint('Button: $val index: $i $selected'),
-                            // 버튼이 선택될 때 디버그 로그 출력
-                            if (selected)
-                              {
-                                choice_list.add(val), // 선택된 버튼을 리스트에 추가
-                                selectCount++,// 선택된 취향 개수 증가
-                              }
-                            else
-                              {
-                                choice_list.remove(val),
-                                // 선택 취소된 버튼을 리스트에서 제거
-                                debugPrint('$choice_list'),
-                                // 선택된 취향 리스트를 디버그 로그로 출력
-                                selectCount--, // 선택된 취향 개수 감소
-                              }
-                          }),
+                      borderRadius: BorderRadius.circular(10),
+                      spacing: 5,
+                      // 버튼 사이의 간격
+                      buttonWidth: MediaQuery.of(context).size.width / 2.4,
+                    ),
+                    isRadio: false,
+                    controller: _controller, // 생성한 controller 할당
+                    onSelected: (val, i, selected) {
+                      setState(() {
+                        if (selected) {
+                          choiceList.add(val); // 선택된 버튼을 리스트에 추가
+                          selectCount++; // 선택된 취향 개수 증가
+                        } else {
+                          choiceList.remove(val);
+                          // 선택 취소된 버튼을 리스트에서 제거
+                          debugPrint('$choiceList');
+                          // 선택된 취향 리스트를 디버그 로그로 출력
+                          selectCount--; // 선택된 취향 개수 감소
+                        }
+                      });
+                    },
+                  ),
                 ),
                 const SizedBox(
                   height: 30,
@@ -102,7 +110,7 @@ class FilterScreen extends StatelessWidget {
                     minimumSize: const Size(200, 56),
                   ),
                   onPressed: () {
-                    debugPrint('$choice_list'); // 선택된 취향 리스트를 디버그 로그로 출력
+                    debugPrint('$choiceList'); // 선택된 취향 리스트를 디버그 로그로 출력
                     Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -113,7 +121,7 @@ class FilterScreen extends StatelessWidget {
                     );
                   },
                   child: Text(
-                    '$selectCount개 검색'.toUpperCase(), // 검색 버튼 텍스트
+                    '키워드 $selectCount개에 대한 검색'.toUpperCase(), // 검색 버튼 텍스트
                   ),
                 ),
               ],
