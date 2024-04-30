910b56506e76e78880839eac2e2e66adc4e2f56a
kangdaehun <crol0101@naver.com>
2024-04-30 14:58:08 +0900

    test

--- lib/Screens/Kategorie/KatClassic_screen.dart
+++ lib/Screens/Kategorie/KatClassic_screen.dart
@@ -9,20 +9,21 @@ class ClassicScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
+        resizeToAvoidBottomInset: true,
         appBar: AppBar(
           title: Text('Classic'),
           backgroundColor: Color(0xFF8A9352),
         ),
         body: SizedBox(
           width: double.infinity,
-          height: MediaQuery
-              .of(context)
-              .size
-              .height,
-          child: Text('new'),
+          height: MediaQuery.of(context).size.height,
+          child: const Column(
+            children: [
+              Text('data'),
+              Text('data')
+            ],
+          ),
         ),
-        bottomNavigationBar: Bottom()
-    );
+        bottomNavigationBar: Bottom());
   }
 }
