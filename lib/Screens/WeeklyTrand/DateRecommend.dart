d59a72cfa722e26c2e9fc22c907755826b90127c
taeuri <xodudwns6251@gmail.com>
2024-05-06 02:21:33 +0900

    요일 날씨 별점 재료 별 추천 배너 만드는중

---
+++ lib/Screens/WeeklyTrand/DateRecommend.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DateScreen extends StatelessWidget {
+  const DateScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/Screens/WeeklyTrand/StarRecommend.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StarScreen extends StatelessWidget {
+  const StarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
