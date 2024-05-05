f8b5efa0c3d04b1dcfb400a69fd79d344cb8b71a
kangdaehun <crol0101@naver.com>
2024-05-05 12:40:38 +0900

    본체로 옮기기

--- lib/Screens/Categorie/KatClassic_screen.dart
+++ lib/Screens/Categorie/KatClassic_screen.dart
@@ -1,27 +1,54 @@
+import 'package:caki_project/Components/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../Main_veiw/Bottom_main.dart';
 
 class ClassicScreen extends StatelessWidget {
-  const ClassicScreen({super.key});
+  List<String> board_data = ['1', '2', '3'];
+  ClassicScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset: true,
         appBar: AppBar(
-          title: Text('Classic'),
-          backgroundColor: Color(0xFF8A9352),
+          title: const Text('Classic'),
+          backgroundColor: kColor,
         ),
-        body: SizedBox(
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height,
-          child: const Column(
-            children: [
-              Text('data'),
-              Text('data')
-            ],
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height,
+            child: ListView.builder(
+              itemCount: board_data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        foregroundColor: Colors.black,
+                        maximumSize: const Size(double.infinity, 100),
+                        minimumSize: const Size(double.infinity, 100),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Image.asset(
+                            "assets/Img/intro_under.png",
+                            fit: BoxFit.fill,
+                          ),
+                          const Spacer(
+                            flex: 1,
+                          ),
+                          Text(board_data[index]),
+                        ],
+                      )),
+                );
+              },
+            ),
           ),
         ),
         bottomNavigationBar: Bottom());
