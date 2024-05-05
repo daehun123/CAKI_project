534e20b567a6ab67c898cc966a05e8a60e8f3453
crol0101@naver.com <crol0101@naver.com>
2024-05-05 16:18:08 +0900

    카테고리 폼 틀 데이터 받아야댐

--- lib/Screens/Categorie/KatClassic_screen.dart
+++ lib/Screens/Categorie/KatClassic_screen.dart
@@ -1,11 +1,11 @@
 import 'package:caki_project/Components/constants.dart';
+import 'package:caki_project/Screens/Categorie/components/board_list_classic.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../Main_veiw/Bottom_main.dart';
 
 class ClassicScreen extends StatelessWidget {
-  List<String> board_data = ['1', '2', '3'];
   ClassicScreen({super.key});
 
   @override
@@ -16,41 +16,7 @@ class ClassicScreen extends StatelessWidget {
           title: const Text('Classic'),
           backgroundColor: kColor,
         ),
-        body: SingleChildScrollView(
-          child: SizedBox(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height,
-            child: ListView.builder(
-              itemCount: board_data.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        foregroundColor: Colors.black,
-                        maximumSize: const Size(double.infinity, 100),
-                        minimumSize: const Size(double.infinity, 100),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Image.asset(
-                            "assets/Img/intro_under.png",
-                            fit: BoxFit.fill,
-                          ),
-                          const Spacer(
-                            flex: 1,
-                          ),
-                          Text(board_data[index]),
-                        ],
-                      )),
-                );
-              },
-            ),
-          ),
-        ),
+        body: BoardList(),
         bottomNavigationBar: Bottom());
   }
 }
