054f86cbcbe9ccf4118c389cbd1a2e29968722d7
crol0101@naver.com <crol0101@naver.com>
2024-05-08 22:22:17 +0900

    위치 정보 및 회원가입 로그인 백 연동

--- lib/Screens/Login/Components/login_form.dart
+++ lib/Screens/Login/Components/login_form.dart
@@ -1,12 +1,35 @@
 import 'package:caki_project/Screens/Preference/preference_screen.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-import '../../Welcome/welcome_screen.dart';
+class LoginForm extends StatefulWidget {
+  LoginForm({super.key});
 
-class LoginForm extends StatelessWidget {
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
   final login_formkey = GlobalKey<FormState>();
 
-  LoginForm({super.key});
+  String? _email, _password;
+
+  Future<void> login(String email, String password) async {
+    var url = Uri.parse('');
+    var response = await http.post(url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+        }));
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      print(data['message']);
+    } else {
+      print('로그인 실패');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +41,7 @@ class LoginForm extends StatelessWidget {
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
             cursorColor: const Color(0xFF8A9352),
+            onSaved: (value) => _email = value,
             decoration: const InputDecoration(
               hintText: 'E-mail',
               prefixIcon: Padding(
@@ -38,6 +62,7 @@ class LoginForm extends StatelessWidget {
               textInputAction: TextInputAction.done,
               obscureText: true,
               cursorColor: const Color(0xFF8A9352),
+              onSaved: (value) => _password = value,
               decoration: const InputDecoration(
                 hintText: 'PassWord',
                 prefixIcon: Padding(
@@ -62,12 +87,13 @@ class LoginForm extends StatelessWidget {
                 child: ElevatedButton(
                   onPressed: () {
                     if (login_formkey.currentState!.validate()) {
+                      login_formkey.currentState!.save();
+                      //login(_email!, _password!);
                       Navigator.pushAndRemoveUntil(
                           context,
                           MaterialPageRoute(
-                              builder: (BuildContext context) =>
-                                  pre_choice()),
-                              (route) => false);
+                              builder: (BuildContext context) => pre_choice()),
+                          (route) => false);
                     }
                   },
                   child: Text('Login'.toUpperCase()),
