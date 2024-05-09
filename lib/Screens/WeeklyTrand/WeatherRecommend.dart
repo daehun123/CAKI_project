15a851bbf8c94d1624da85f58bf3e5ae333f8e25
kangdaehun <crol0101@naver.com>
2024-05-09 10:54:39 +0900

    위치 전송

--- lib/Screens/WeeklyTrand/WeatherRecommend.dart
+++ lib/Screens/WeeklyTrand/WeatherRecommend.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../Main_veiw/Bottom_main.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
@@ -33,6 +35,24 @@ class _WeatherScreenState extends State<WeatherScreen> {
       latitude = position.latitude.toString();
       longitude = position.longitude.toString();
     });
+
+    Future<void> shareLocation() async {
+      final response = await http.post(
+        Uri.parse('http://127.0.0.1:8000/search?q=독도'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'latitude' : latitude!,
+          'longitude' : longitude!,
+        }),
+      );
+      if(response.statusCode == 200){
+        print('전송 완료');
+      }else{
+        print('전송 실패');
+      }
+    }
   }
 
   @override
